b02b8aa5f1341bccc2d0faba0e351639aba1cc52
Aryan <as2280@srmist.edu.in>
2023-03-18 15:58:48 +0530

    new commit

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
 //ctrl+d
-let firstCard = getRandomCard();
-let secondCard = getRandomCard();
-let sum = firstCard + secondCard;
-let card = [firstCard, secondCard]; // array of cards
+let firstCard;
+let secondCard;
+let sum = 0;
+let card = []; // array of cards
 let hasBlackjack = false;
-let isAlive = true;
+let isAlive = false;
 console.log(sum);
 let messageEl = document.getElementById("message-El");
 let sumEl = document.getElementById("message-Sum");
 let cardsEl = document.getElementById("message-Cards");
 let message = "";
+let messagePer = document.getElementById("message-Per");
+let player = {
+  name: "Per",
+  chips: 145,
+};
+messagePer.textContent = player.name + " $" + player.chips;
 function startGame() {
+  firstCard = getRandomCard();
+  secondCard = getRandomCard();
+  sum = firstCard + secondCard;
+  card.push(firstCard);
+  card.push(secondCard);
+  isAlive = true;
   renderGame();
 }
 function getRandomCard() {
-  return Math.floor(Math.random() * 13) + 1;
+  let val = Math.floor(Math.random() * 13) + 1;
+  if (val === 1) return 11;
+  else if (val >= 11 && val <= 13) return 10;
+  else return val;
 }
 function renderGame() {
   if (sum <= 20) {
@@ -35,18 +50,28 @@ function renderGame() {
   }
 }
 function newCard() {
-  let newcard = getRandomCard();
-  sum += newcard;
-  card.push(newcard);
-  console.log(sum);
-  renderGame();
+  if (isAlive === true && hasBlackjack === false) {
+    let newcard = getRandomCard();
+    sum += newcard;
+    card.push(newcard);
+    console.log(sum);
+    renderGame();
+  }
 }
 
-let featuredPosts = [
-  "chekck out my nwe new card",
-  "here is a new card",
-  "I have a new card",
-];
-let newmesaages = "Hi there";
-featuredPosts.push(newmesaages);
-console.log(featuredPosts);
+let person = {
+  name: "Aryan",
+  age: "21",
+  country: "India",
+};
+
+function logData() {
+  console.log(
+    person.name +
+      " is " +
+      person.age +
+      " years old and lives in " +
+      person.country
+  );
+}
+logData();
